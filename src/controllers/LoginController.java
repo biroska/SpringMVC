c3ea8dbdaf9fcae271da9fc8206c51f39cffc8ea
biroska <biroska@gmail.com>
2014-10-24 16:28:02 -0200

    Inclusão do Log4J

--- src/controllers/LoginController.java
+++ src/controllers/LoginController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpSession;
 
 import modelo.Usuario;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +17,17 @@ public class LoginController {
 	@Autowired
 	FacadeImpl facade;
 	
+	final static Logger logger = Logger.getLogger(LoginController.class);
+	
 	@RequestMapping("logar")
 	public String logar( Usuario user, HttpSession session ){
 		System.out.println("LoginController.logar(): " + user.getUsuario() +" - "+ user.getSenha() );
+		
+		logger.info("This is an info log entry: " + user.getUsuario() +" - "+ user.getSenha() );
+		logger.warn("This is warn : " + user.getUsuario() +" - "+ user.getSenha() );
+		logger.error("This is error : " + user.getUsuario() +" - "+ user.getSenha() );
+		logger.fatal("This is fatal : " + user.getUsuario() +" - "+ user.getSenha() );
+		
 		if ( facade.autenticausuarioSenha( user ) ){
 			session.setAttribute("usuarioLogado", user );
 			return "forward:principal";
